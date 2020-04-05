f9c79f2b86c5c34fec610b407d00b9dc09ed7cfa
rajsudeep <sudrocks@gmail.com>
2020-04-04 17:16:20 -0700

    Create example component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
+import ButtonExample from './components/UI/ButtonExample';
 import './App.css';
 
 function App() {
+  const buttonStyles = {
+    container: {
+      width: 200, 
+      height: 40, 
+      borderRadius: 20, 
+      display: 'flex', 
+      justifyContent: 'center'
+    },
+    text: {
+      fontSize: 16,
+      margin: 0,
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +25,12 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <ButtonExample 
+          onClick={() => console.log('Hi')}
+          text={'Button'} 
+          buttonStyle={buttonStyles.container}
+          textStyle={buttonStyles.text}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/components/UI/ButtonExample.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+interface IProps {
+    buttonStyle?: object;
+    textStyle?: object;
+    text?: string;
+    onClick?: () => void;
+};
+
+const Button: React.FC<IProps> = props => {
+    let { buttonStyle, textStyle, text, onClick } = props;
+
+    return (
+        <button style={buttonStyle} onClick={onClick}>
+            <p style={textStyle}>{text}</p>
+        </button>
+    )
+};
+
+export default Button;
